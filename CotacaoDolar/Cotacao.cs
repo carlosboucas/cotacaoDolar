173179842645a8cdb84fc95ff7c95bf374dcb70b
Carlos Bouças <carlos.rodrigo.cmb@gmail.com>
2022-08-20 16:36:31 -0300

    cotação dólar

---
+++ CotacaoDolar/Cotacao.cs
@@ -0,0 +1,16 @@
+﻿namespace CotacaoDolar
+{
+    class Cotacao
+    {
+        public static double cot = 0.06;
+        public static double cotDolar = 0;
+        public static double compReais = 0;
+
+        public static double CalcCotacao(double cotDolar, double compReais)
+        {
+            double mult = cotDolar * compReais;
+            double cotacao = mult * cot;
+            return mult + cotacao;
+        }
+    }
+}
---
+++ CotacaoDolar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDolar
+{
+    class Program
+    {
+            static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólas? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double reais = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = Cotacao.CalcCotacao(dolar, reais);
+
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
